f5bcbbac61c175f903d743779d158af465f35b5b
bhanuprabhas <83233294+bhanuprabhas@users.noreply.github.com>
2021-06-03 18:28:12 +0530

    Create arithmetic by int and str.py

---
+++ arithmetic by int and str.py
@@ -0,0 +1,12 @@
+num_int=4
+num_str=2
+#here the condition is str converts to int
+num_str=int(num_str)
+add=num_int+num_str
+sub=num_int-num_str
+mul=num_int*num_str
+div=num_int/num_str
+print("the sum of num_int and num_str is:",add)
+print("the min of num_int and num_str is:",sub)
+print("the mul of num_int and num_str is:",mul)
+print("the div of num_int and num_str is:",div)
